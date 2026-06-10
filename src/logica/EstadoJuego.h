fa1e7eee260acc829c1613d8de379ce7427816e8
fonteg42-beep <fonteg42@gmail.com>
2026-06-10 11:35:54 -0400

    Add files via upload

---
+++ src/logica/EstadoJuego.h
@@ -0,0 +1,17 @@
+#ifndef ESTADOJUEGO_H
+#define ESTADOJUEGO_H
+
+enum class EstadoJuego {
+    EN_CURSO,
+    GANADOR_X,
+    GANADOR_O,
+    EMPATE
+};
+
+enum class Pantalla {
+    MENU,
+    JUGANDO,
+    RESULTADO
+};
+
+#endif
